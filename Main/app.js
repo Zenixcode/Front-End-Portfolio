2939829d6cf2a1b6a7b27e3b94f5ef51cbf22ef2
Zenixcode <nathanhenry43@gmail.com>
2021-03-23 17:52:10 +0200

    Hamburger Nav added

---
+++ Main/app.js
@@ -0,0 +1,14 @@
+(function navSlide(){
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-ul');
+    const navLinks = document.querySelector('.nav-ul li');
+
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    });
+
+
+    
+    
+})();
